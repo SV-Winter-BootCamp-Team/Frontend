afc64952435d50b89b63e6df88ca991b339e0c78
yjun0915 <110004440+yjun0915@users.noreply.github.com>
2024-01-04 01:35:39 +0900

    feat: 온보딩 페이지 퍼블리싱

--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -1,3 +1,17 @@
+import OnBoardingTemplate from "../components/OnBoarding";
+
 export default function OnBoardingPage() {
-	return <div>OnBoarding Page</div>
+	return (
+		<div>
+            <header className="fixed top w-[100vw] h-[70px]">
+				<div className="bg-[#00000066] w-[100vw] h-[70px] px-[30px] py-[15px]">
+                	<div className="text-white text-3xl">꾸며Zoom</div>
+				</div>
+				{/* <div className="bg-gradient-to-b from-[#CB96EF] to-[#7AA1CD] h-[100vh] z-0" /> */}
+            </header>
+				<OnBoardingTemplate />
+		</div>
+	);
 }
+
+//className="bg-gradient-to-b from-[#CB96EF] to-[#7AA1CD] h-[100vh]"
