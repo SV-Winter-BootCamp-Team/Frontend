ddda4e5bd69925be5a314b0eb035e2c4e0ce2005
rachel4w2 <rachel4w2@naver.com>
2024-01-04 03:35:53 +0900

    feat: 캔버스 반응형 그리드 레이아웃

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,3 +1,13 @@
-export default function Canvas() {
-	return <div>Canvas</div>
+import { CanvasDataType } from '../../pages/MainPage'
+
+export default function Canvas({ canvas_name, update_at }: CanvasDataType) {
+	return (
+		<div className="flex-col">
+			<div className="flex justify-center py-32 border-2 px-28 sm:py-28">
+				<p>Viewer</p>
+			</div>
+			<div>{canvas_name}</div>
+			<div>{update_at}</div>
+		</div>
+	)
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,68 @@
+import { useParams } from 'react-router-dom'
+import Canvas from '../components/Canvas'
+import { useEffect, useState } from 'react'
+
+export type CanvasDataType = {
+	canvas_id: number
+	canvas_name: string
+	update_at: string
+}
+
 export default function MainPage() {
-	return <div>Main Page</div>
+	const user_id = useParams()
+	const [canvasData, setCanvasData] = useState<CanvasDataType[]>([])
+
+	// TODO: fetch user data from server
+
+	const response = {
+		message: '개인 캔버스 전체 조회 성공',
+		result: {
+			canvases: [
+				{
+					canvas_id: 1,
+					canvas_name: 'Untitled',
+					update_at: '2023-01-05',
+				},
+				{
+					canvas_id: 2,
+					canvas_name: 'Untitled2',
+					update_at: '2023-01-06',
+				},
+				{
+					canvas_id: 3,
+					canvas_name: 'Untitled3',
+					update_at: '2023-01-07',
+				},
+				{
+					canvas_id: 4,
+					canvas_name: 'Untitled4',
+					update_at: '2023-01-07',
+				},
+				{
+					canvas_id: 5,
+					canvas_name: 'Untitled5',
+					update_at: '2023-01-07',
+				},
+				{
+					canvas_id: 6,
+					canvas_name: 'Untitled6',
+					update_at: '2023-01-07',
+				},
+			],
+		},
+	}
+
+	useEffect(() => {
+		setCanvasData(response.result.canvases)
+	}, [])
+
+	console.log(canvasData)
+
+	return (
+		<div className="grid grid-cols-1 gap-8 mx-8 mt-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+			{canvasData.map((canvas: CanvasDataType) => (
+				<Canvas key={canvas.canvas_id} {...canvas} />
+			))}
+		</div>
+	)
 }
