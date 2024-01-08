96f7aa3fd3c8d8e15c3f6b4bb883c45e9ab21aa2
yjun0915 <110004440+yjun0915@users.noreply.github.com>
2024-01-08 18:18:25 +0900

    style: 섹션 하나로 통합

--- src/components/OnBoarding/OnBoardingTemplate.tsx
+++ src/components/OnBoarding/OnBoardingTemplate.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 export default function OnBoardingTemplate() {
 	return (
 		<div>
-			<section className="w-[100vw] h-[100vh] flex-col bg-gradient-to-b from-[#CB96EF] to-[#7AA1CD]">
-				<div className="w-fit pt-[358px] mx-auto text-[#514958] text-6xl">
+			<section className="w-[100vw] h-fit flex-col bg-gradient-to-b from-[#CB96EF] to-[#7AA1CD]">
+				<div className="w-fit pt-[358px] mx-auto text-[#514958] text-[60px]">
 					<h1>꾸며Zoom에서 당신만의 배경을 꾸며보세요</h1>
 				</div>
 				<div className="mt-28 w-fit mx-auto">
@@ -12,24 +12,20 @@ export default function OnBoardingTemplate() {
 						<Link to="/signup">시작하기</Link>
 					</button>
 				</div>
-			</section>
-			<section className="w-[100vw] h-[100vh] flex-col bg-gradient-to-b from-[#7AA1CD] to-[#CB96EF]">
-				<div className="grid grid-cols-3 gap-4 px-32 h-[60vh]">
-					<div className="border-2 border-[#000000] border-dotted rounded-[25px]" />
-					<div className="border-2 border-[#000000] border-dotted rounded-[25px]" />
-					<div className="border-2 border-[#000000] border-dotted rounded-[25px]" />
-					<div className="border-2 border-[#000000] border-dotted rounded-[25px]" />
-					<div className="border-2 border-[#000000] border-dotted rounded-[25px]" />
-					<div className="border-2 border-[#000000] border-dotted rounded-[25px]" />
+				<div className="grid grid-cols-3 gap-4 px-32 h-[60vh] mt-80">
+					<div className="bg-[#ddd] rounded-[25px]" />
+					<div className="bg-[#ddd] rounded-[25px]" />
+					<div className="bg-[#ddd] rounded-[25px]" />
+					<div className="bg-[#ddd] rounded-[25px]" />
+					<div className="bg-[#ddd] rounded-[25px]" />
+					<div className="bg-[#ddd] rounded-[25px]" />
 				</div>
 				<div className="pt-24 w-fit mx-auto text-[#514958] text-5xl">
 					<div>
 						이미 수많은 사용자들이 매일 창의적인 작품들을 만들어내고 있습니다!
 					</div>
 				</div>
-			</section>
-			<section className="w-[100vw] h-[100vh] flex-col bg-gradient-to-b from-[#CB96EF] to-[#7AA1CD]">
-				<div className="w-fit pt-[163px] mx-auto text-[#514958] text-6xl">
+				<div className="w-fit pt-[163px] mx-auto text-[#514958] text-6xl mt-32">
 					<div className="border-2 border-[#000000] border-dotted rounded-[25px] w-[700px] h-[600px]" />
 				</div>
 				<div className="w-fit mx-auto text-[#514958] text-5xl">
