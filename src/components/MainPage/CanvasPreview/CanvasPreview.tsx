2647c30b1b2058634c5b0c443fa052aa93532bc4
yjun0915 <110004440+yjun0915@users.noreply.github.com>
2024-01-11 20:38:22 +0900

    feat: 이름 수정, 삭제 기능 생성

--- src/components/MainPage/CanvasPreview/CanvasPreview.tsx
+++ src/components/MainPage/CanvasPreview/CanvasPreview.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { CanvasPreviewProps } from '../../../pages/MainPage'
 
 export default function CanvasPreview({
@@ -5,12 +6,40 @@ export default function CanvasPreview({
 	canvas_name,
 	update_at,
 }: CanvasPreviewProps) {
+	const [hide, setHide] = useState(false)
+	const [changeName, setChangeName] = useState(canvas_name)
+
 	return (
 		<div className="flex-col">
-			<div className="flex justify-center px-32 py-32 border-2 sm:py-[100px]">
+			<div
+				className="relative flex justify-center px-32 py-32 border-2 sm:py-[100px]"
+				onMouseEnter={() => {
+					setHide(true)
+				}}
+				onMouseLeave={() => {
+					setHide(false)
+				}}
+			>
 				<p>{content}</p>
+				<button
+					className={`absolute top-0 right-0 ml-44 text-white ${
+						!hide && 'hidden'
+					}`}
+				>
+					❌
+				</button>
 			</div>
-			<div>{canvas_name}</div>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault()
+					console.log(changeName)
+				}}
+			>
+				<input
+					onChange={(e) => setChangeName(e.target.value)}
+					value={changeName}
+				/>
+			</form>
 			<div>{update_at}</div>
 		</div>
 	)
