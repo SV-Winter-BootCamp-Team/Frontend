55f8f480ca24e5598fc8987eb26d88fe661e75fa
rachel4w2 <rachel4w2@naver.com>
2024-01-16 00:02:31 +0900

    feat: 추천 배경 API 연동

--- src/components/CanvasPage/RecommendBackground/RecommendBackground.tsx
+++ src/components/CanvasPage/RecommendBackground/RecommendBackground.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
 export default function RecommendBackground() {
-	return <div>RecommendBackground</div>
+	const params = useParams<{ canvas_id: string }>()
+	const [backgrounds, setBackgrounds] = useState([])
+
+	useEffect(() => {
+		const fetchRecommendedBackgrounds = async () => {
+			try {
+				const response = await axios.get(
+					`http://localhost:8000/api/v1/canvases/${params.canvas_id}/backgrounds/recommend/`,
+				)
+				console.log('Recommended backgrounds:', response.data)
+				setBackgrounds(response.data.results)
+			} catch (error) {
+				console.error('Error fetching recommended backgrounds:', error)
+			}
+		}
+
+		fetchRecommendedBackgrounds()
+	}, [])
+
+	return (
+		<div className="flex flex-col items-center mt-8">
+			{backgrounds.map((background, index) => (
+				<img
+					key={index}
+					src={background}
+					className="w-[304px] h-[171px] mb-2"
+				/>
+			))}
+		</div>
+	)
 }
