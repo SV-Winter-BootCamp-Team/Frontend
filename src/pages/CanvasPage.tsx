30d515e682481272c167a620d86d13aec7e1740c
rachel4w2 <rachel4w2@naver.com>
2024-01-16 00:02:31 +0900

    feat: 추천 배경 API 연동

--- src/pages/CanvasPage.tsx
+++ src/pages/CanvasPage.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import html2canvas from 'html2canvas'
 
 import NavBar from '../components/CanvasPage/NavBar'
 import Canvas from '../components/CanvasPage/Canvas'
 import MenuBar from '../components/CanvasPage/MenuBar'
 import MenuSection from '../components/CanvasPage/MenuSection'
+import axios from 'axios'
+import { useParams } from 'react-router'
 
 export type MoveableElement = {
 	id: string
@@ -14,12 +16,15 @@ export type MoveableElement = {
 }
 
 export default function CanvasPage() {
+	const params = useParams<{ canvas_id: string }>()
 	const [isOpen, setIsOpen] = useState(false)
 	const [selectedMenu, setSelectedMenu] = useState<string>('내 캔버스')
 
 	const [backgroundURL, setBackgroundURL] = useState<string>('')
 
 	const [componentList, setComponentList] = useState<MoveableElement[]>([])
+
+	const [canvasName, setCanvasName] = useState<string>('')
 
 	const handleAddComponent = (componentURL: string) => {
 		const newElement: MoveableElement = {
@@ -56,9 +61,27 @@ export default function CanvasPage() {
 		}
 	}
 
+	useEffect(() => {
+		const fetchCanvasDetails = async () => {
+			try {
+				const response = await axios.get(
+					`http://localhost:8000/api/v1/canvases/detail/${params.canvas_id}/`,
+				)
+				setCanvasName(response.data.result.canvas_name)
+				setComponentList(response.data.result.components)
+				console.log(response.data.result.canvas_name)
+				console.log(response.data.result.components)
+			} catch (error) {
+				console.error('Error fetching canvas details:', error)
+			}
+		}
+
+		fetchCanvasDetails()
+	}, [])
+
 	return (
 		<div className="flex flex-col min-h-screen">
-			<NavBar captureCanvas={captureCanvas} />
+			<NavBar captureCanvas={captureCanvas} canvasName={canvasName} />
 			<div className="flex flex-grow" style={{ height: 'calc(100vh - 55px)' }}>
 				<div className="flex h-full bg-white">
 					<MenuBar
