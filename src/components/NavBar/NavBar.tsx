ada09687eb2d05c4de002bfda82704a95cd3e14a
yjun0915 <110004440+yjun0915@users.noreply.github.com>
2024-01-23 15:11:38 +0900

    style: 메인페이지 nav bar 수정

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,11 @@
 export default function NavBar() {
 	return (
-		<div className="flex items-center h-12 pl-4 bg-purple-200">
-			<h1 className="text-gray-600">꾸며Zoom</h1>
+		<div className="flex items-center h-12 pl-4 bg-[#66CAE1]">
+			<h1 className="text-white font-jua text-3xl">꾸며Zoom</h1>
 			<button
 				className="font-jua text-xl text-[#66CAE1] bg-white rounded-lg py-1 px-2 ml-auto mr-4 border hover:border-[#31BFCF]"
 				onClick={() => {
-					window.location.replace(`http://localhost:5173`)
+					window.location.replace(`/`)
 					localStorage.clear()
 				}}
 			>
