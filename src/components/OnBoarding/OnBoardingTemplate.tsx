9db28783253bee0fdbfc1069e5fb0d94636b500d
yjun0915 <110004440+yjun0915@users.noreply.github.com>
2024-01-27 13:41:47 +0900

    refactor: 이미지 슬라이더 모듈화

--- src/components/OnBoarding/OnBoardingTemplate.tsx
+++ src/components/OnBoarding/OnBoardingTemplate.tsx
@@ -5,12 +5,7 @@ import lottie from 'lottie-web'
 import webSocketJson from '../../animations/json/webSocket.json'
 import startJson from '../../animations/json/startImage.json'
 import PathDrawing from './PathDrawing'
-
-export type ImageType = {
-	key: number
-	src: string
-	alt: string
-}
+import ImageSlider from './ImageSlider'
 
 export type HandleThreeType = {
 	color: string
@@ -18,117 +13,6 @@ export type HandleThreeType = {
 }
 
 export default function OnBoardingTemplate() {
-	const images1: ImageType[] = [
-		{
-			key: 1,
-			src: '/images/svg/examples/example01.svg',
-			alt: 'onBoarding1',
-		},
-		{
-			key: 2,
-			src: '/images/svg/examples/example02.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 3,
-			src: '/images/svg/examples/example03.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 4,
-			src: '/images/svg/examples/example04.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 5,
-			src: '/images/svg/examples/example05.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 6,
-			src: '/images/svg/examples/example06.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 7,
-			src: '/images/svg/examples/example07.svg',
-			alt: 'onBoarding2',
-		},
-	]
-	const images2: ImageType[] = [
-		{
-			key: 1,
-			src: '/images/svg/examples/example11.svg',
-			alt: 'onBoarding1',
-		},
-		{
-			key: 2,
-			src: '/images/svg/examples/example12.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 3,
-			src: '/images/svg/examples/example13.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 4,
-			src: '/images/svg/examples/example14.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 5,
-			src: '/images/svg/examples/example15.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 6,
-			src: '/images/svg/examples/example16.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 7,
-			src: '/images/svg/examples/example17.svg',
-			alt: 'onBoarding2',
-		},
-	]
-	const images3: ImageType[] = [
-		{
-			key: 1,
-			src: '/images/svg/examples/example21.svg',
-			alt: 'onBoarding1',
-		},
-		{
-			key: 2,
-			src: '/images/svg/examples/example22.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 3,
-			src: '/images/svg/examples/example23.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 4,
-			src: '/images/svg/examples/example24.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 5,
-			src: '/images/svg/examples/example25.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 6,
-			src: '/images/svg/examples/example26.svg',
-			alt: 'onBoarding2',
-		},
-		{
-			key: 7,
-			src: '/images/svg/examples/example27.svg',
-			alt: 'onBoarding2',
-		},
-	]
 	const [handleThree, setHandleThree] = useState<HandleThreeType>({
 		color: '#80B9BF',
 		x: 0,
@@ -267,60 +151,7 @@ export default function OnBoardingTemplate() {
 						</div>
 						]
 					</div>
-					<div className="flex flex-row-reverse gap-[1%] mt-[3%] overflow-hidden">
-						{images1.map((image) => (
-							<img
-								className="w-[15%] animate-slider"
-								key={image.key}
-								alt={image.alt}
-								src={image.src}
-							/>
-						))}
-						{images1.map((image) => (
-							<img
-								className="w-[15%] animate-slider"
-								key={image.key}
-								alt={image.alt}
-								src={image.src}
-							/>
-						))}
-					</div>
-					<div className="flex flex-row gap-[1%] mt-4 overflow-hidden">
-						{images2.map((image) => (
-							<img
-								className="w-[15%] animate-reverse_slider"
-								key={image.key}
-								alt={image.alt}
-								src={image.src}
-							/>
-						))}
-						{images2.map((image) => (
-							<img
-								className="w-[15%] animate-reverse_slider"
-								key={image.key}
-								alt={image.alt}
-								src={image.src}
-							/>
-						))}
-					</div>
-					<div className="flex flex-row-reverse gap-[1%] mt-4 overflow-hidden">
-						{images3.map((image) => (
-							<img
-								className="w-[15%] animate-slider"
-								key={image.key}
-								alt={image.alt}
-								src={image.src}
-							/>
-						))}
-						{images3.map((image) => (
-							<img
-								className="w-[15%] animate-slider"
-								key={image.key}
-								alt={image.alt}
-								src={image.src}
-							/>
-						))}
-					</div>
+					<ImageSlider />
 				</div>
 				<div className="relative h-[190vh]">
 					<div className="h-[60vh] w-full flex justify-between items-center px-[10%]">
