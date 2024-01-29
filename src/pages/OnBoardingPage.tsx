7f81e4a653b8d9b6c8e5969593e7a1478427e132
yjun0915 <110004440+yjun0915@users.noreply.github.com>
2024-01-29 14:46:16 +0900

    style: 사이드바 네비바로 수정

--- src/pages/OnBoardingPage.tsx
+++ src/pages/OnBoardingPage.tsx
@@ -1,12 +1,37 @@
+import { useNavigate } from 'react-router-dom'
 import OnBoardingTemplate from '../components/OnBoarding'
-import SideBar from './SideBar'
 
 export default function OnBoardingPage() {
+	const nav = useNavigate()
 	return (
-		<div className="w-full flex max-w-full overflow-hidden">
-			<div className="w-1/6">
-				<SideBar />
-			</div>
+		<div className="w-screen max-w-full overflow-hidden">
+			<header className="z-10 fixed top w-full h-[70px] flex justify-between text-white bg-[#fff] px-[30px] py-[15px] border-gray-200 border-b-[1px]">
+				<div className="font-jua text-[#66CAD1] text-4xl">꾸며Zoom</div>
+				<div>
+					<div className="flex font-sans font-nomal text-white">
+						<button
+							className="rounded-lg py-[11px] px-5 text-[13px] mx-4 flex items-center bg-cyan-50 text-[#66CAD1] cative:bg-cyan-600 hover:bg-[#66CAD1] hover:text-white"
+							onClick={() => {
+								nav({
+									pathname: '/signup',
+								})
+							}}
+						>
+							회원가입
+						</button>
+						<button
+							className="rounded-lg py-[11px] px-5 text-[13px] mx-4 flex items-center bg-cyan-50 text-[#66CAD1] cative:bg-cyan-600 hover:bg-[#66CAD1] hover:text-white"
+							onClick={() => {
+								nav({
+									pathname: '/login',
+								})
+							}}
+						>
+							로그인
+						</button>
+					</div>
+				</div>
+			</header>
 			<OnBoardingTemplate />
 		</div>
 	)
